b9e092a7ead3fb03df541f5b14439e4da8db2f8e
mingu <alsrn7590@naver.com>
2022-03-08 16:08:37 +0900

    initial commit

---
+++ src/main/java/mcdonald/mcdeliveryapi/McdeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package mcdonald.mcdeliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McdeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McdeliveryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mcdonald/mcdeliveryapi/McdeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mcdonald.mcdeliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McdeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
