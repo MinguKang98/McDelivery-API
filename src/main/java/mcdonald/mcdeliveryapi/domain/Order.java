dce9a183665ea24c5871125817381dfe08c8b9e4
mingu <alsrn7590@naver.com>
2022-03-17 17:03:39 +0900

    feat: Add orderItems in Order

--- src/main/java/mcdonald/mcdeliveryapi/domain/Order.java
+++ src/main/java/mcdonald/mcdeliveryapi/domain/Order.java
@@ -3,6 +3,8 @@ package mcdonald.mcdeliveryapi.domain;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,9 +19,12 @@ public class Order {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL) //DeliveryRepository 를 따로 생성하지 않고 영속성 관리
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
+
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<OrderItem>();
 
     private int expectedTime;
 
