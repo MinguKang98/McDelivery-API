31a639ceda0ff765a4cc79b32bd9bbc7c71db308
mingu <alsrn7590@naver.com>
2022-03-21 11:00:06 +0900

    feat: Add Repositories

--- src/main/java/mcdonald/mcdeliveryapi/repository/OrderRepository.java
+++ src/main/java/mcdonald/mcdeliveryapi/repository/OrderRepository.java
@@ -1,2 +1,29 @@
-package mcdonald.mcdeliveryapi.repository;public class OrderRepository {
+package mcdonald.mcdeliveryapi.repository;
+
+import lombok.RequiredArgsConstructor;
+import mcdonald.mcdeliveryapi.domain.Order;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findById(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    public List<Order> findAll() {
+        return em.createQuery("select o from Order o", Order.class)
+                .getResultList();
+    }
+
 }
